8abf3d4a8a52377db67bfc9cb2217fca902c2beb
copyrighttxt <rlxwreal@gmail.com>
2025-04-22 18:37:22 -0400

    Update badwords.py

--- badwords.py
+++ badwords.py
@@ -205,7 +205,56 @@ BadWords = [
     "austiblox.net",
     "kamari.zip",
     "reddit.com", #kiddos post random stuff sometimes
-    "drive.google.com"
+    "drive.google.com",
+    "kill urself",
+    "killurself",
+    " r.a.pe",
+    "rаpe", #russian character(s)
+    "r.аpe", #russian character(s)
+    "rа.pe", #russian character(s)
+    "r.а.p.e", #russian character(s)
+    "r.а.pe", #russian character(s)
+    "r.аp.e", #russian character(s)
+    "rаpе", #russian character(s)
+    "r.аpе", #russian character(s)
+    "rа.pе", #russian character(s)
+    "r.а.p.е", #russian character(s)
+    "r.аp.е", #russian character(s)
+    "kіll yourself", #russian character(s)
+    "kіll yоursеlf", #russian character(s)
+    "fаggot", #russian character(s)
+    "fаggоt", #russian character(s)
+    "nіggеr", #russian character(s)
+    "nіgger", #russian character(s)
+    "rеtard", #russian character(s)
+    "rеtаrd", #russian character(s)
+    "r.еtаrd", #russian character(s)
+    "r.е.tаrd", #russian character(s)
+    "r.е.t.аrd", #russian character(s) 
+    "r.е.t.а.rd", #russian character(s) 
+    "r.е.t.а.r.d", #russian character(s)
+    "r.еtard", #russian character(s)
+    "r.е.tard", #russian character(s)
+    "r.е.t.ard", #russian character(s)
+    "r.е.t.a.rd", #russian character(s)
+    "r.е.t.a.r.d", #russian character(s)
+    "rе.tard", #russian character(s)
+    "rеt.ard", #russian character(s)
+    "rеta.rd", #russian character(s)
+    "rе.tard", #russian character(s)
+    "r.еtar.d", #russian character(s)
+    "rеtar.d", #russian character(s)
+    "fаg", #russian character(s)
+    "f.аg", #russian character(s)
+    "fа.g", #russian character(s)
+    "f.а.g", #russian character(s)
+    "n.іggеr", #russian character(s)
+    "nі.ggеr", #russian character(s)
+    "nіg.gеr", #russian character(s)
+    "nіgg.еr", #russian character(s)
+    "nіggе.r", #russian character(s)
+    "n.і.g.g.е.r", #russian character(s)
+    "kyѕ" #russian character(s)
 ]
 
 ExtendedBadWords = [
